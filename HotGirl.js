116a2151f8778ada3362e968f1f1ab6444ae8d6e
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-31 20:21:48 +0700

    base

--- HotGirl.js
+++ HotGirl.js
@@ -1,7 +1,24 @@
+const queryDB = require('./db');
+
 class HotGirl {
-    static getGirlById(id) {
-        
+    static async getGirlById(id) {
+        const selectSql = 'SELECT * FROM "HotGirl" WHERE id = $1';
+        const result = await queryDB(selectSql, [id]);
+        if (!result.rows[0]) throw new Error('Hotgirl khong ton tai');
+        return result.rows[0];
     }
+    // static getGirlById(id) {
+    //     const selectSql = 'SELECT * FROM "HotGirl" WHERE id = $1';
+    //     return queryDB(selectSql, [id])
+    //     .then(result => {
+    //         if (!result.rows[0]) return Promise.reject(new Error('Hotgirl khong ton tai'));
+    //         return result.rows[0];
+    //     });
+    // }
 }
 
 module.exports = HotGirl;
+
+// HotGirl.getGirlById(10)
+// .then(girl => console.log(girl))
+// .catch(err => console.log(err.message));
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
-
+const HotGirl = require('./HotGirl');
 const app = express();
 app.listen(3000, () => console.log('Server started'));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-app.get('/', (req, res) => res.render('home'));
+// app.get('/', (req, res) => res.render('home'));
 
 app.get('/hotgirl/:id', (req, res) => {
-    
+    const { id } = req.params;
+    HotGirl.getGirlById(id)
+    .then(girl => res.render('home', { girl }))
+    .catch(err => res.send(err.message));
 });
